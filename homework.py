25e8011f381f05780e614e2193795baa449945d4
Pati <ppeczynska@gmail.com>
2020-02-15 23:17:52 +0100

    first tasc from second page min- not working yet

--- homework.py
+++ homework.py
@@ -1,11 +1,34 @@
-#hello world
+#hello world function returning string
 def hello_world():
-    return ("Hello, World!")
-name=str("PAT")
+    return ('Hello, World!')
+
+name="Pati"
+
 #hello name
 def hello(name):
-    return "Hello " + name
+    return ('Hello ' + name +"!")
+    if len(name)>0:
+        hello(name)
+    else:
+        hello_world()
 
 #print hello
-print (hello(name))
+def print_hello(name):
+    print(hello(name))
 
+
+print_hello(name)
+
+#terminal: python3 homework.py
+#onemore optional task
+
+x=input()
+y=input()
+
+def min(x,y):
+    if x<y:
+        return(x)
+    else:
+        return(y)
+
+min(x,y)
