97dfd9dbbb159df0d0af143f8c8659907f804693
Pati <ppeczynska@gmail.com>
2020-02-16 20:41:38 +0100

    second task added first 3 functions

--- homework.py
+++ homework.py
@@ -2,33 +2,46 @@
 def hello_world():
     return ('Hello, World!')
 
-name="Pati"
+
+name ="Pati"
 
 #hello name
 def hello(name):
-    return ('Hello ' + name +"!")
+    return('Hello ' + name +"!")
     if len(name)>0:
         hello(name)
     else:
         hello_world()
 
+
 #print hello
 def print_hello(name):
     print(hello(name))
-
 
 print_hello(name)
 
 #terminal: python3 homework.py
 #onemore optional task
 
-x=input()
-y=input()
+#zapytać Piotra czy input to jest ta zakazana built-in function?
+x= int(input("please write any real number"))
+y= int(input("please write second real number"))
 
-def min(x,y):
-    if x<y:
-        return(x)
-    else:
-        return(y)
+#min(x,y)
+if x<y:
+    print(x)
+else:
+    print(y)
 
-min(x,y)
+
+#max(x,y)
+if x>y:
+    print(x)
+else:
+    print(y)
+
+
+#len
+iterable=len(input("please type any word"))
+
+print(iterable)
