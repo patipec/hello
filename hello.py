9d0b25bc140c21fd4e778cf9dba0c66611b7e3ce
Pati <ppeczynska@gmail.com>
2020-02-23 15:52:17 +0100

    po poprawkach od Damiana

--- hello.py
+++ hello.py
@@ -6,46 +6,50 @@ name = "Pati"
 
 
 # hello name
-def hello(name):
-    return('Hello ' + name + "!")
+def hello(name=""): #nadanie zmiennej default argument
     if len(name) > 0:
-        hello(name)
+        return('Hello ' + name + "!")
     else:
-        hello_world()
+        return hello_world()
 
+
+print(hello())
 
 # print hello
-def print_hello(name):
+def print_hello(name=""): # musi byc ta defaultowa wartosc nadana gdyby nic nie bylo jako zmienna name
     print(hello(name))
 
 print_hello(name)
 
-# terminal: python3 homework.py
+# terminal: python3 hello.py
 # onemore optional task
 
-x = int(input("please write any real number "))
-y = int(input("please write second real number "))
+x = int(input("please write any natural number "))
+y = int(input("please write second natural number "))
 
 # min(x,y)
-if x < 0:
-    print("I said REAL NUMBER")
-elif x < y:
+
+if x < y:
     print("Less number is: " + str(x))
 else:
     print("Less number is: " + str(y))
 
 
 # max(x,y)
-if x < 0:
-    print("I said REAL NUMBER")
-elif x > y:
+
+if x > y:
     print("Greater number is: " + str(x))
 else:
     print("Greater number is: " + str(y))
-3
+
 
 # len
 iterable = len(input(str("please type any word ")))
+def len(iterable):
+    lenght = 0
+    for i in iterable:
+        lenght +=1
+        return lenght
 print("Your word has " +str(iterable) + " signs")
 
 # multiply
@@ -54,6 +58,8 @@ y = int(input("Please give second real number "))
 
 def multiply(x, y):
     if x ==0:
+        return 0
+    elif y ==0:
         return 0
     else:
         return y + multiply(y, x-1)
