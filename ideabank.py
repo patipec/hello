879dc708e108086e092066e314d74c4abbd68d55
Pati <ppeczynska@gmail.com>
2020-03-01 16:21:37 +0100

    first verion of ideabank but not last

--- ideabank.py
+++ ideabank.py
@@ -1,5 +1,22 @@
-idea=[]
-idea=input("What is your new idea?")
+import sys
+import io
+import os
 
-def adding(idea):
-    print(idea)
+def adding():
+    print("What is your new idea?")
+    idea = input()
+    return(idea)
+
+added = adding()
+text_file = open("ideas.txt", "w")  # otwiera plik tekstowy 
+# w wersji edytowalnej
+text_file.write(added)  # wpisuje input do otwartego pliku
+text_file.close()  # zamyka plik
+
+# wyciaganie idei z pliku i printowanie
+ideas = open("ideas.txt", "r")
+lines = list(ideas)
+ideas.close()
+print(len(lines), ".", lines)
+
+
